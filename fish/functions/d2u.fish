54a746b0786447592c8b391d7797d14916966e13
Hieu Cao <hieu.cao@enotion.io>
2025-05-05 14:18:58 +0700

    feat: convert dos2unix

---
+++ fish/functions/d2u.fish
@@ -0,0 +1,19 @@
+function d2u -d "Convert all files in a folder from CRLF to LF"
+    if test (count $argv) -eq 0
+        echo "Usage: convert-dos2unix <directory>"
+        return 1
+    end
+
+    set -l target (realpath $argv[1])
+
+    if not test -d $target
+        echo "Error: '$target' is not a valid directory"
+        return 1
+    end
+
+    echo "Converting all files under $target to Unix line endings..."
+
+    find $target -type f -print0 | xargs -0 dos2unix --
+
+    echo "Done."
+end
