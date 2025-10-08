87c070ffc3bff72fbb02e77c342320b9e23b94bd
Hieu Cao <hieu.cao@seatudio.com>
2025-10-08 09:49:13 +0700

    update fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -6,14 +6,18 @@ if status is-interactive
 
     zoxide init fish | source
     fzf --fish | source
-    source (pyenv init -|psub)
+    jenv init - | source
+    pyenv init - fish | source
 
     set -gx EDITOR /usr/local/bin/code
     set -gx VISUAL $EDITOR
 
     set -gx FZF_DEFAULT_OPTS '--style=full --layout=reverse --border --preview="~/.config/fzf/preview.sh {}"'
 
-    set -l libs curl llvm node openjdk@17
+    # set -gxa LDFLAGS '-isysroot $(xcrun --show-sdk-path)'
+    # set -gxa CPPFLAGS '-isysroot $(xcrun --show-sdk-path)'
+
+    set -l libs curl llvm node node@22 openjdk@17 gettext
     for lib in $libs
         set -l base /opt/homebrew/opt/$lib
         if test -d $base/lib
@@ -23,6 +27,9 @@ if status is-interactive
             set -gxa CPPFLAGS -I$base/include
         end
     end
+
+    set -gx ANDROID_HOME /Users/fe-hieu/Library/Android/sdk
+    set -gx PATH /Users/fe-hieu/Library/Android/sdk/emulator /Users/fe-hieu/Library/Android/sdk/cmdline-tools/latest/bin /Users/fe-hieu/Library/Android/sdk/platform-tools $PATH
 
     test -e "$HOME/.iterm2_shell_integration.fish"; and source "$HOME/.iterm2_shell_integration.fish"
 end
